2af91481d1774c2bd5c2100ebaa26d821206f088
skarunak <senthilkumar.karunakaran@gmail.com>
2016-04-22 08:05:21 -0700

    list iter,formula corrected

--- UCSC/Assignments/A0.py
+++ UCSC/Assignments/A0.py
@@ -8,13 +8,13 @@ def BuildHist(input_arr, bin):
    min_num = min(input_arr)
    max_num = max(input_arr)
 
-   for i in input_arr:
-     pos = (int)(math.ceil((bin-1) * ((float)(max_num - input_arr[i])/max_num)))
+   for i,val in enumerate(input_arr):
+     pos = (int)(math.ceil((bin-1) * ((float)(input_arr[i]-min_num)/(max_num-min_num))))
      out[pos] = out[pos] + 1
 
    return out
 
 bin = 10
-inp = list(range(100))
+inp = list(range(100,200))
 hist = BuildHist(inp, bin)
 print hist
