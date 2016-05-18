a3e58dc9821fa785c51adefa8f6b79bbd958098f
skarunak <senthilkumar.karunakaran@gmail.com>
2016-05-17 22:28:04 -0700

    workgin

--- UCSC/Assignments/3/A3.py
+++ UCSC/Assignments/3/A3.py
@@ -26,72 +26,14 @@ def BuildGender2dBayes(male_arr, female_arr):
 
    m_cov = np.cov(male_heights, male_handspan)
    f_cov = np.cov(female_heights, female_handspan)
-   #print "GetMeanCov ..."
-   #print m_mean
-   #print f_mean
-   #print (m_cov)
-   #print (f_cov)
-   #print "Male details ..." 
    m_first = (1/((2*math.pi)*math.sqrt(np.linalg.det(m_cov))))
-   #arg2 = h - m_mean
-   #m_cov_arr = np.array(m_cov)
-   #arg3 = np.linalg.inv(m_cov) 
-   #arg2_arr = np.matrix(arg2) 
-   #arg4 = arg2_arr.transpose()
-   #arg5 = np.matrix(arg2) * np.matrix(arg3)
-   #print "Details ..."
-   #print h
-   #print len(male_arr)
-   #print arg1
-   #print arg2
-   #print arg3
-   #print arg4 
-   #print arg5
-   #print (arg5 * np.matrix(arg4))
-   #print math.exp((-0.5)*124)
-   #p_male = len(male_arr) * arg1 *math.exp((-0.5)*(np.matrix(arg2) * np.matrix(arg3) * np.matrix(arg4)))
 
    f_first = (1/((2*math.pi)*math.sqrt(np.linalg.det(f_cov))))
-   #arg2 = (h-f_mean)
-   #f_cov_arr = np.array(f_cov)
-   #arg3 = np.linalg.inv(f_cov) 
-   #arg2_arr = np.matrix(arg2) 
-   #arg4 = arg2_arr.transpose()
-   #arg5 = np.matrix(arg2) * np.matrix(arg3)
-
-   #p_female = len(male_arr) * arg1 *math.exp((-0.5)*(np.matrix(arg2) * np.matrix(arg3) * np.matrix(arg4)))
-
-   #print ("p output %f %f " % (p_male, p_female))
-
-   # Probabilities ..
-   #prob_male = p_male / (p_male + p_female)
-   #prob_female = p_female / (p_male + p_female)
 
    return m_mean, m_cov, m_first, f_mean, f_cov, f_first
 
 def PredictGender2dBayes(male_arr, female_arr, h, m_mean, m_cov, m_first, f_mean, f_cov, f_first):
 
-   #male_heights = []
-   #male_handspan = []
-   #female_heights = []
-   #female_handspan = []
-
-   #male_heights.append([list_var[0] for list_var in male_arr])
-   #male_handspan.append([list_var[1] for list_var in male_arr])
-   #female_heights.append([list_var[0] for list_var in female_arr])
-   #female_handspan.append([list_var[1] for list_var in female_arr])
-
-   #m_mean = np.mean(male_arr, axis=0)
-   #f_mean = np.mean(female_arr, axis=0)
-
-   #m_cov = np.cov(male_heights, male_handspan)
-   #f_cov = np.cov(female_heights, female_handspan)
-   #print "GetMeanCov ..."
-   #print m_mean
-   #print f_mean
-   #print (m_cov)
-   #print (f_cov)
-   #print "Male details ..." 
    arg1 = (1/((2*math.pi)*math.sqrt(np.linalg.det(m_cov))))
    arg2 = h - m_mean
    m_cov_arr = np.array(m_cov)
@@ -99,16 +41,6 @@ def PredictGender2dBayes(male_arr, female_arr, h, m_mean, m_cov, m_first, f_mean
    arg2_arr = np.matrix(arg2) 
    arg4 = arg2_arr.transpose()
    arg5 = np.matrix(arg2) * np.matrix(arg3)
-   #print "Details ..."
-   #print h
-   #print len(male_arr)
-   #print arg1
-   #print arg2
-   #print arg3
-   #print arg4 
-   #print arg5
-   #print (arg5 * np.matrix(arg4))
-   #print math.exp((-0.5)*124)
    p_male = len(male_arr) * arg1 *math.exp((-0.5)*(np.matrix(arg2) * np.matrix(arg3) * np.matrix(arg4)))
 
    arg1 = (1/((2*math.pi)*math.sqrt(np.linalg.det(f_cov))))
@@ -121,8 +53,6 @@ def PredictGender2dBayes(male_arr, female_arr, h, m_mean, m_cov, m_first, f_mean
 
    p_female = len(male_arr) * arg1 *math.exp((-0.5)*(np.matrix(arg2) * np.matrix(arg3) * np.matrix(arg4)))
 
-   #print ("p output %f %f " % (p_male, p_female))
-
    # Probabilities ..
    prob_male = p_male / (p_male + p_female)
    prob_female = p_female / (p_male + p_female)
@@ -133,8 +63,6 @@ def PredictGender2dBayes(male_arr, female_arr, h, m_mean, m_cov, m_first, f_mean
        P = 'Female'
    else :
        P = 'Indeterminate'
-
-   #print ("Bayes:: Pred: %s p_male: %f p_female: %f" % ( P, prob_male, prob_female))
 
    return prob_male, prob_female
 
@@ -163,7 +91,6 @@ def PredictGender2dHist(h, hs, male_hist, bin_h, min_h, max_h, female_hist, bin_
    else :
        P = 'Indeterminate'
 
-   #print ("Hist :: h: %d hs: %d ; pos: %d,%d Pred: %s m_cnt: %d ; f_cnt: %d MP: %f FP: %f" % (h, hs, pos_h, pos_hs, P, male_cnt, female_cnt, p_male, p_female))
    return p_male, p_female
 
 def Build2dHist(input_arr, bin_h, min_h, max_h, bin_hs, min_hs, max_hs):
@@ -329,15 +256,6 @@ second = 4
 third = 1
 
 lbl_img_list, lbl1, lbl2, lbl3 = get_lables('training', first,second, third)
-'''
-for lbl,vec in read():
-   if (lbl == 3 or lbl == 4):
-      lbl_img_list.append((lbl, vec))
-      if (lbl == 3):
-         lbl1.append((lbl, vec))
-      if (lbl == 4):
-         lbl2.append((lbl, vec))
-'''
 lbl_img_list = sorted(lbl_img_list, key=lambda key: key[0])
 print ("Data set: sz %d #of-%d %d #of-%d %d" % (len(lbl_img_list), first, len(lbl1), second, len(lbl2)))
 
@@ -373,9 +291,12 @@ V_row, V_col = V.shape
 print ("V : (%d, %d) " % (V_row, V_col))
 
 # P
-V = np.matrix(V).transpose()
-TV = V.transpose()
-P = Z * TV
+#V = np.matrix(V).transpose()
+V = np.matrix(V)
+#TV = V.transpose()
+#TV = V
+#P = Z * TV
+P = Z * V
 P_mean = np.mean(P, axis=0)
 #print "P_mean ..", P_mean
 
@@ -466,26 +387,3 @@ print ("Gaus result (AllDigs): correct %d wrong %d Indet %d " % (correct, wrong,
 #PredictGender2dBayes(P1P2_dig1, P1P2_dig2, test_p1p2)
 print "done .."
 #plt.show()
-'''
-correct = 0
-wrong = 0
-indet = 0
-
-for lbl, vec in test_inp:
-   test_p1p2 = getp1p2oftest(vec, mean, V)
-   p_dig1 , p_dig2 = PredictGender2dHist(test_p1p2[0,0], test_p1p2[0,1], dig1_hist, p1_bin, p1_min, p1_max, dig2_hist, p2_bin, p2_min, p2_max)
-
-   if p_dig1 > p_dig2:
-      out = first
-   elif p_dig1 < p_dig2:
-      out = second
-   else :
-      out = -1
-
-   if (out == lbl):
-      correct = correct + 1
-   elif (out == -1):
-      indet = indet + 1
-   else:
-      wrong = wrong + 1
-'''
